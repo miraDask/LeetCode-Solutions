3e9ab701af325d93abb964b256c1b470f0d90564
miraDask <mira.daskalov@gmail.com>
2020-09-03 11:44:10 +0100

    add git ignore and new problem : Min stack

---
+++ Min Stack/Solution 1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solution_1
+{
+    public class MinStack
+    {
+        private List<int> storage;
+        /** initialize your data structure here. */
+        public MinStack()
+        {
+            this.storage = new List<int>();
+        }
+
+        public void Push(int x)
+        {
+            this.storage.Add(x);
+        }
+
+        public void Pop()
+        {
+            this.storage.RemoveAt(this.storage.Count - 1);
+        }
+
+        public int Top()
+        {
+            return this.storage[this.storage.Count - 1];
+        }
+
+        public int GetMin()
+        {
+            return this.storage.Min(x => x);
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var comands = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            var minStack = new MinStack();
+
+            for (int i = 0; i < comands.Length; i++)
+            {
+                switch (comands[i])
+                {
+                    case "top":
+                        Console.WriteLine(minStack.Top());
+                        break;
+                    case "pop":
+                        minStack.Pop();
+                        break;
+                    case "getMin":
+                        Console.WriteLine(minStack.GetMin());
+                        break;
+                    default:
+                        var value = int.Parse(comands[i].Split(" ")[1]);
+                        minStack.Push(value);
+                        break;
+                }
+            }
+        }
+    }
+}
+// test:
+//push -2, push 0, push -3, getMin, pop, top, getMin 
---
+++ Min Stack/Solution 2 - faster/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solution_2___faster
+{
+
+    public class MinStack
+    {
+        private Stack<int> storage = new Stack<int>();
+        private Stack<int> minValues = new Stack<int>();
+
+        public void Push(int x)
+        {
+            this.storage.Push(x);
+            if (minValues.Count == 0 || x <= minValues.Peek())
+            {
+                minValues.Push(x);
+            }
+        }
+
+        public void Pop()
+        {
+            var last =  this.storage.Pop();
+            if (last == minValues.Peek())
+            {
+                minValues.Pop();
+            }
+        }
+
+        public int Top()
+        {
+            return this.storage.Peek();
+        }
+
+        public int GetMin()
+        {
+            return this.minValues.Peek();
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var comands = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            var minStack = new MinStack();
+
+            for (int i = 0; i < comands.Length; i++)
+            {
+                switch (comands[i])
+                {
+                    case "top":
+                        Console.WriteLine(minStack.Top());
+                        break;
+                    case "pop":
+                        minStack.Pop();
+                        break;
+                    case "getMin":
+                        Console.WriteLine(minStack.GetMin());
+                        break;
+                    default:
+                        var value = int.Parse(comands[i].Split(" ")[1]);
+                        minStack.Push(value);
+                        break;
+                }
+            }
+        }
+    }
+}
