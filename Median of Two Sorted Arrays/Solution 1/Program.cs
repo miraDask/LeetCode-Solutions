176ef494cb4cce76c99a095286179140f95edb94
miraDask <mira.daskalov@gmail.com>
2020-09-19 16:53:19 +0100

    add new problem Median of Two Sorted Arrays

---
+++ Median of Two Sorted Arrays/Solution 1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Solution_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums1 = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            var nums2 = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            Console.WriteLine(FindMedianSortedArrays(nums1, nums2));
+        }
+
+        public static double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            var lengthOfNewArray = nums1.Length + nums2.Length;
+            var newArray = new int[lengthOfNewArray];
+
+            for (var i = 0; i < nums1.Length; i++)
+            {
+                newArray[i] = nums1[i];
+            }
+
+            var pointer = 0;
+            for (var i = nums1.Length; i < lengthOfNewArray; i++)
+            {
+                newArray[i] = nums2[pointer++];
+            }
+
+            Array.Sort(newArray);
+
+            if (lengthOfNewArray % 2 == 0)
+            {
+                var middle = lengthOfNewArray / 2;
+                return (newArray[middle] + newArray[middle - 1]) / 2.00000;
+            }
+            else
+            {
+                return newArray[lengthOfNewArray / 2] * 1.00000;
+            }
+        }
+    }
+}
