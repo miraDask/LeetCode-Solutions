57ef4d790ff8b9122d51fd2abbdd5e7d876e6567
miraDask <mira.daskalov@gmail.com>
2020-09-21 18:46:44 +0100

    add new problem: Evaluate Reverse Polish Notation

---
+++ Evaluate Reverse Polish Notation/Solution 1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solution_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(", ").ToArray();
+            var calculationResult = EvalRPN(input);
+            Console.WriteLine(calculationResult);
+        }
+
+        public static int EvalRPN(string[] tokens)
+        {
+            var stack = new Stack<int>();
+
+            for (var i = 0; i < tokens.Length; i++)
+            {
+                var current = tokens[i];
+                var isNumber = int.TryParse(current, out int number);
+
+                if (isNumber)
+                {
+                    stack.Push(number);
+                }
+                else
+                {
+                    var secondElement = stack.Pop();
+                    var firstElement = stack.Pop();
+
+                    var tempResult = current switch
+                    {
+                        "-" => firstElement - secondElement,
+                        "+" => firstElement + secondElement,
+                        "/" => firstElement / secondElement,
+                        "*" => firstElement * secondElement,
+                        _ => throw new NotImplementedException()
+                    };
+
+                    stack.Push(tempResult);
+                }
+            }
+
+            return stack.Pop();
+        }
+    }
+}
+
+// test inputs:
+//4, 13, 5, /, +
+//2, 1, +, 3, *
