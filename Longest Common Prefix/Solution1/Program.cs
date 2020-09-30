b9b75fb27bedcb5c381df040014549e1ab2c76fd
miraDask <mira.daskalov@gmail.com>
2020-09-30 16:34:26 +0100

    add new problem Longest Common Prefix

---
+++ Longest Common Prefix/Solution1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solution1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(", ").ToArray();
+            Console.WriteLine(LongestCommonPrefix(input));
+        }
+
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            var result = string.Empty;
+            if (strs.Length <= 1)
+            {
+                result = strs.Length == 1 ? strs[0] : result;
+                return result;
+            }
+
+            var indexOfShortest = FindShortest(strs);
+            var shortestString = strs[indexOfShortest] ;
+
+            var rest = strs.Where((x, i) => i != indexOfShortest).ToList(); 
+
+            for (var i = shortestString.Length; i >= 0; i--)
+            {
+                var current = shortestString.Substring(0, i);
+                var existsInAllStrings = true;
+                foreach (var str in rest)
+                {
+                    var x = str.Substring(0, i);
+                    if (x != current)
+                    {
+                        existsInAllStrings = false;
+                        break;
+                    }
+                }
+
+                if (existsInAllStrings)
+                {
+                    result = current;
+                    return result;
+                }
+
+            }
+
+            return result;
+        }
+
+        public static int FindShortest(string[] strs)
+        {
+            var index = -1;
+            var minLenght = int.MaxValue;
+            for (var i = 0; i < strs.Length; i++)
+            {
+                if (strs[i].Length < minLenght)
+                {
+                    minLenght = strs[i].Length;
+                    index = i;
+                }
+            }
+
+            return index;
+        }
+    }
+}
---
+++ Longest Common Prefix/Solution2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace Solution2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(", ").ToArray();
+            Console.WriteLine(LongestCommonPrefix(input));
+        }
+
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            var result = string.Empty;
+
+            if(strs.Length <= 1)
+            {
+                result = strs.Length == 0 ? result : strs[0];
+                return result;
+            }
+
+            var firstString = strs[0];
+
+            for (int i = 0; i < firstString.Length; i++)
+            {
+                var current = firstString[i];
+                var exists = true;
+
+                for (int j = 1; j < strs.Length; j++)
+                {
+                    if (i > strs[j].Length - 1 || strs[j][i] != current)
+                    {
+                        exists = false;
+                        break;
+                    }
+                    
+                }
+
+                if(exists)
+                {
+                    result += current;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
