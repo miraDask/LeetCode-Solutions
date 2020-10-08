58f3c1453e8b0268b062449281b2c9a214585d73
miraDask <mira.daskalov@gmail.com>
2020-10-08 16:55:27 +0100

    add new problem: String to Integer (atoi)

---
+++ String to Integer (atoi)/Solution1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Solution1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            Console.WriteLine(MyAtoi(input));
+        }
+
+        public static int MyAtoi(string s)
+        {
+            if (string.IsNullOrEmpty(s) || string.IsNullOrWhiteSpace(s))
+            {
+                return 0;
+            }
+
+            var toArr = s.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            var first = toArr[0];
+            var sub = string.Empty;
+
+            for (var i = 0; i < first.Length; i++)
+            {
+                if (first[i] == '-' && i == 0)
+                {
+                    sub += first[i];
+                    continue;
+                }
+                if (first[i] == '+' && i == 0)
+                {
+                    continue;
+                }
+
+                if (first[i] == '0' && sub.Length < 1)
+                {
+                    continue;
+                }
+
+                if (Char.IsDigit(first[i]))
+                {
+                    sub += first[i];
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (sub.Length == 0 || sub.Length == 1 && !Char.IsDigit(sub[0]))
+            {
+                return 0;
+            }
+
+            int result = 0;
+
+            try
+            {
+                result = int.Parse(sub);
+            }
+            catch
+            {
+
+                if (sub[0] == '-')
+                {
+                    result = int.MinValue;
+                }
+                else
+                {
+                    result = int.MaxValue;
+                }
+            }
+            
+            return result;
+        }
+    }
+}
